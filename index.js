f4b870966508075fbb6597eacad0ed5ae21a6484
romashka <kazka826@gmail.com>
2024-12-18 18:42:27 +0200

    done

---
+++ index.js
@@ -0,0 +1,32 @@
+// let cel = prompt("Cels");
+// let far = prompt("Fahrenheit");
+// console.log((cel * 9)/5 + 32);
+
+// let month = prompt("days per month");
+// console.log(month * 24);
+// console.log(month * 1440);
+
+
+// let health =prompt("your health");
+// let energ =prompt("your energy");
+// console.log(health - 10);
+// console.log(energ - 18);
+
+// let shop = prompt("enter your number");
+// console.log(shop - (shop * (1/10)));
+
+// let number = prompt("enter your number");
+// console.log(Math.floor(number));
+
+let text = prompt("enter your number");
+console.log(parseFloat(text));
+
+// let num1 = prompt("enter your number");
+// console.log(parseInt(num1));
+
+// let num2 = prompt("enter your number");
+// console.log(Math.sqrt(num2));
+
+// let num3 = prompt("enter your number");
+// console.log(parseInt(num3));
+// console.log(toString(num3));
