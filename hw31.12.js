ab4ca321ec66c81f976f74fc7dd360e6aff0a9b2
romashka <kazka826@gmail.com>
2024-12-30 18:59:15 +0200

    done hw 31.12

---
+++ hw31.12.js
@@ -0,0 +1,50 @@
+// -----------1-------------
+// const smth1 =prompt("smth");
+// const smth2 =prompt("smth2");
+// if (smth1.length !== 0 && smth2.length !== 0){
+// alert( "Обидва поля заповнені");
+// }
+// else{
+//     alert( "Не всі поля заповнені");
+// };
+
+// --------------2---------------
+// const num1 =Number(prompt("num1"));
+// const num2 =Number(prompt("num2"));
+// const sum= num1 + num2;
+// if( sum >10){
+//     alert("Сума більша за 10")
+// }
+// else{
+//     alert("Сума менша або дорівнює 10")
+// }
+
+// -----------------3--------------
+// const smth =prompt("smth");
+
+// if(smth.includes("JavaScript")){
+//     alert("Текст містить слово JavaScript")
+// }
+// else{
+//     alert("Текст не містить слово JavaScript")
+// }
+
+// -----------------4-----------------
+// let num1 =prompt('ur number');
+// if( 10 < num1 && num1 < 20){
+//     alert( "Число входить в діапазон від 10 до 20")
+// }
+// else{
+//     alert( "Число не входить в діапазон від 10 до 20")
+// }
+
+// -------------------5-----------------
+// let urName=prompt("ur name");
+// let email=prompt("ur email");
+// let password=prompt("ur password))");
+// if( urName.length>3 && email.includes("@.") && password.length > 6){
+//     alert("Перенаправлення на іншу сторінку")
+// }
+// else{
+//     alert("Помилка: неправильне заповнення")
+// }
