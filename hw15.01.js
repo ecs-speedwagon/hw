88663e3fc8ad6cb03a6ceac8d5c29cba3753b5d5
romashka <kazka826@gmail.com>
2025-01-17 17:15:04 +0200

    hw 4 16.01

---
+++ hw15.01.js
@@ -0,0 +1,60 @@
+// let x = 0;
+// while(x !==10){
+//     x+=1;
+//     console.log(x)
+// }
+
+
+// -----------2-----------
+//  let x=20;
+//  for(let i = 2; i <= x; i +=2){
+// console.log(i);
+//  }
+
+// -------------3----------
+//  let x = prompt("number");
+//  let result = 0;
+//  for(let i = 7; i > 0; i +=0){
+//     result= i *x;
+    
+//     console.log(result);
+//     break;
+//  }
+
+
+
+// ----------6-----------
+// let n = prompt("n number");
+// let urNum;
+// do{
+//     urNum=Number(prompt("ur num"));
+//     if(n >= urNum){
+//         break
+
+//     }
+//    else{
+   
+    
+//    }
+//    console.log(urNum)
+// }
+// while(urNum === n); 
+
+
+// for(let i = 0; i < n; i+=1){
+//     console.log(i);
+// }
+
+
+// // ------------7---------
+// let x = 1;
+
+// while(x <= 20) {
+//     if(x % 3===0){
+//         x +=1;
+//         continue
+//     }
+// console.log(x);
+// x+=1;
+
+// }
