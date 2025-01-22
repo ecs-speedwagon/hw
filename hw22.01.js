d61c9c277c339717325479e61f890661d040f493
romashka <kazka826@gmail.com>
2025-01-22 16:32:23 +0200

    hw 22 01

---
+++ hw22.01.js
@@ -0,0 +1,60 @@
+// let x = [1,2,3];
+// x[2]=10;
+// console.log(x[2]);
+// let massive= ["zxc", "asd", "qwerty", "sigma"];
+// massive[4]="ww";
+// console.log(massive[4]);
+// let x = [1,2,3,4,5,6,7,8,9,10,11];
+// let xresult=0;
+
+// for(let i = 0; i < x.length; i +=1){
+//     xresult+=x[i];
+// }
+// console.log(xresult);
+
+// let x = [1,2,3,4,5];
+// for (let y of x){
+//     console.log(y);
+// }
+
+
+// let massive= ["zxc", "asd", "qwertdsy", "sigma"];
+// let mass
+// for(let i=0; i < massive.length; i++){
+     
+//     if(massive[i].length>=5){
+//     console.log(massive[i]);
+//     break;
+//     }
+//     else{
+//     continue
+//     }
+    
+// }
+
+
+// тут я хз
+// let x =[21,134,1,72,412,3,43,213,42,41];
+// let y 
+// for(let i=0; i < x.length; i++){
+//     if(x[i] > x[i+1]){
+//         y = x[i];
+//         console.log(Math.max(y));
+//         break
+// }
+// }
+
+
+
+
+
+
+
+
+// let num =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+// for(let i=0; i < num.length; i +=1){
+//     if(num[i]%2 ===0){
+//         console.log(num[i]);
+//       continue
+//     }
+// }
