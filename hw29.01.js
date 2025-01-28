a169326ee37995d4b1d1d34fbdc74827db4dda2b
romashka <kazka826@gmail.com>
2025-01-28 21:47:46 +0200

    added hw 29.01

---
+++ hw29.01.js
@@ -0,0 +1,25 @@
+// let x = ["Hello world welcome to Paris"];
+// console.log(x.join(' '));
+
+// for (var i = 0; i < x.length; i++) {
+//     console.log(x[i]);
+// }
+
+// const cards =[
+//     "card-1",
+//     "card-2",
+//     "card-3",
+//     "card-4",
+//     "card-5",
+// ];
+// // console.log(cards.splice(2,1));
+// // console.log(cards.splice(5,0, "card-6"));
+// // console.log(cards.splice(3,1, "card-444"));
+// // console.log(cards)
+
+// console.log(cards.indexOf("card-5",));
+
+
+
+
+
