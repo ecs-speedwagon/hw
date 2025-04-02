04de409d2d2e68c7b78d22166dbd9d28a7d17108
romashka <kazka826@gmail.com>
2025-04-02 15:09:48 +0300

    hw 02.04

---
+++ hw02.04.js
@@ -0,0 +1,19 @@
+// const input = document.querySelector('input');
+// const button = document.querySelector('button');
+
+// button.addEventListener('click', () => {
+//    button.textContent = input.value;
+// });
+
+
+// const img =document.querySelector('.imgPatric');
+// img.src="/img/bob and patric.jpg";
+
+
+// const img = document.querySelector('img');
+// const a = document.querySelector('a');
+// a.href = 'https://youtu.be/hzt31eJTGxo?si=RrViee7VDbnmULKg';
+// img.alt = 'bob';
+
+// const fChild= document.querySelector('.child');
+// fChild.textContent ="asdasdadsasd";
